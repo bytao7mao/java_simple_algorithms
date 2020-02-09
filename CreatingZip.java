7bef74aa5d8b4af8a82de058e31c989d70253e24
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2020-02-10 00:52:56 +0200

    creating zip added

---
+++ CreatingZip.java
@@ -0,0 +1,10 @@
+StringBuilder sb = new StringBuilder();
+sb.append("Test String");
+File f = new File("d:\\test.zip");
+ZipOutputStream out = new ZipOutputStream(new FileOutputStream(f));
+ZipEntry e = new ZipEntry("mytext.txt");
+out.putNextEntry(e);
+byte[] data = sb.toString().getBytes();
+out.write(data, 0, data.length);
+out.closeEntry();
+out.close();
