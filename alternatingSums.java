6fc4f0a206aad8a63aeb19d3caee7f04e092664c
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2020-02-13 23:18:39 +0200

    Update alternatingSums.java

--- alternatingSums.java
+++ alternatingSums.java
@@ -3,6 +3,11 @@ static int[] alternatingSums(int[] a) {
         int[] x = new int[2];
         int a1 =0,a2 = 0;
         for (int i=0;i<a.length;i++){
+            //if iteration is even the add 
+            //the number to
+            //the first integer
+            //if odd add to the second
+            //integer
             if (i%2==0){
                 a1 += a[i];
             } else {
